dd3bbcac89057e2bf0126cc55f9685190478b485
Thomas-Af <thomas.afonso@mail-ecv.fr>
2022-06-09 13:37:04 +0200

    first commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-wzk*ww1g4(w@x8s8n%0gfc*go9i(r95(yv62_g7z6dk#4p1$+2"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
